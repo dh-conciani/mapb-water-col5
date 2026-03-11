c1949a0215b8f8587c52fb465dd3c84a33e975b1
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2026-03-11 11:46:13 -0300

    Create getAnnual.js

---
+++ getAnnual.js
@@ -0,0 +1,121 @@
+var geometry = 
+    ee.Geometry.Polygon(
+        [[[-62.46630829754432, -1.7087981026668047],
+          [-62.46630829754432, -26.702057803681825],
+          [-39.96630829754433, -26.702057803681825],
+          [-39.96630829754433, -1.7087981026668047]]], null, false);
+
+var asset = 'projects/nexgenmap/TRANSVERSAIS/AGUA5-FT-CERRADO-COL5';
+//var asset =  'projects/nexgenmap/TRANSVERSAIS/AGUA5-FT'
+
+var cadence = 'annual';
+
+var version = '11'
+
+var obs = 'new'
+//var obs = 'old';
+
+// set year
+var years = ee.List.sequence(1985, 2025).remove(2020).getInfo();
+
+// read territories
+var territory = ee.Image('projects/mapbiomas-workspace/AUXILIAR/biomas-2019-raster')
+  .eq(4).selfMask().rename('territory').aside(Map.addLayer);
+  
+// change the scale if you need.
+var scale = 30;
+
+// define a Google Drive output folder 
+var driverFolder = 'mapb-water-col5-v' + version + 'c';
+
+// Image area in hectares
+var pixelArea = ee.Image.pixelArea().divide(10000);
+
+// convert a complex object to a simple feature collection 
+var convert2table = function (obj) {
+  obj = ee.Dictionary(obj);
+    var territory = obj.get('territory');
+    var classesAndAreas = ee.List(obj.get('groups'));
+    
+    var tableRows = classesAndAreas.map(
+        function (classAndArea) {
+            classAndArea = ee.Dictionary(classAndArea);
+            var classId = classAndArea.get('class');
+            var area = classAndArea.get('sum');
+            var tableColumns = ee.Feature(null)
+                .set('territory', territory)
+                .set('class_id', classId)
+                .set('area', area);
+                
+            return tableColumns;
+        }
+    );
+  
+    return ee.FeatureCollection(ee.List(tableRows));
+};
+
+// compute the area
+var calculateArea = function (image, territory, geometry) {
+    var territotiesData = pixelArea.addBands(territory).addBands(image)
+        .reduceRegion({
+            reducer: ee.Reducer.sum().group(1, 'class').group(1, 'territory'),
+            geometry: geometry,
+            scale: scale,
+            maxPixels: 1e13
+        });
+        
+    territotiesData = ee.List(territotiesData.get('groups'));
+    var areas = territotiesData.map(convert2table);
+    areas = ee.FeatureCollection(areas).flatten();
+    return areas;
+};
+
+var recipe2 = ee.FeatureCollection([])
+
+// for each year
+years.forEach(function(year) {
+  
+  // read annual 
+  var annual = ee.ImageCollection(asset)
+                 .filter(ee.Filter.eq('version', version))
+                 .filter(ee.Filter.eq('cadence', cadence))
+                 .filter(ee.Filter.eq('year', year))
+                 .mosaic()
+                 .select('classification')
+                 
+  //print(year, 'annual bands', annual.bandNames());
+  //Map.addLayer(annual)
+
+  
+  // perform per year 
+  var areas = years.map(
+      function (year) {
+          var image = annual;
+          var areas = calculateArea(image, territory, geometry);
+          // set additional properties
+          areas = areas.map(
+              function (feature) {
+                  return feature.set('year', year)
+                                .set('cadence', cadence)
+                                .set('version', version)
+                                .set('asset', asset)
+                                .set('obs', obs);
+              }
+          );
+          return areas;
+      }
+  );
+    
+
+  areas = ee.FeatureCollection(areas).flatten();
+  
+  recipe2 = recipe2.merge(areas)
+});
+
+Export.table.toDrive({
+        collection: recipe2,
+        description: 'water-' + cadence + '-' + 'CERRADO' + '-' + obs + '-' + 'v' + version,
+        folder: driverFolder,
+        fileFormat: 'CSV'
+    });
+
